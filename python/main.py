c2ac36b0618ab0b97bc96cf8f49212a517bff44a
pranshul08 <pranshulshakya16@gmail.com>
2022-09-11 12:39:28 +0530

    iterative

---
+++ python/main.py
@@ -0,0 +1,27 @@
+# a = int(input('enter a number'))
+# i = 1
+# for i in range (1,a+1):
+# print('hello world')
+
+# s = "jOhNdOe"
+# l = ''
+# u = ''
+# for i in s:
+    # if i.islower():
+        # l = l + i
+    # else:
+        # u += i
+# print(l+u)
+
+# s = 'P@#yn26at^&i5ve'
+# c = 0
+# d = 0
+# sy = 0
+# for i in s:
+    # if i.isalpha():
+        # c = c+ 1
+    # elif i.isdigit():
+        # d = d + 1
+    # else:
+        # sy = sy + 1
+# print(f"chars = {c}\ndigits = {d}\nsymbol = {sy}")
